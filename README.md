522c9cab733ed1e13cde0f9321590797c88910d2
SeaPeaMe <cpuseapeame123@gmail.com>
2025-09-22 22:40:23 +1000

    Changed how files are written, now multiple log files can be written at once and not mess with each other, while still printing to the console.
    Also changed the documentation to reflect this.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This V module make it much easier to:
 - Easily print all logs to a file for further debugging
 
 # Example Usage
+## Print Formatted Text
 Here are some examples on how to print text with different colors and styling:
 ```v
 // Prints "Hello, World!" to the console in blue text
@@ -18,18 +19,16 @@ ezlog.print_formatted("Hello, World!", foreground_color: .red, styling: [ .under
 // Prints "Hello, World!" to the console in bright cyan text that is bold, italicized and blinks
 ezlog.print_formatted("Hello, World!", foreground_color: .bright_cyan, styling: [ .bold, .italic, .blink ])
 ```
+## Print Text With Headings
 Here is the general use of how to use the indentation:
 
 <img width="316" height="108" alt="image" src="https://github.com/user-attachments/assets/bb4f38c4-b666-4586-a814-896164de8e46" />
 
 ```v
-// This is optional, creates a log file relative to the executable's location
-// and writes all uses of log, log_message, etc.
-ezlog.set_log_file("log.txt", "=== BEGIN LOG ===")
 ezlog.start_section_heading("This is my Heading")
 
 ezlog.log_message("Hello, World!")
-ezlog.log_message_formatted("Hello, World In Green!", foreground_color: .green)
+ezlog.log_message("Hello, World In Green!", foreground_color: .green)
 
 ezlog.start_section_heading("Here comes a subheading!")
 ezlog.log_message("Hello, Subheading!")
@@ -38,10 +37,27 @@ ezlog.end_section()
 ezlog.log_message("And we're back to the first heading again!")
 ezlog.end_section()
 ```
+## Caller Names
 This module also generally enforces the use of having a "caller name" in the default log(), log_error(), etc. functions, as to make it easier to track down what is logging to the console.
 ```v
 // This will print:
 // "[Engine]: Starting Engine..."
 // to the console
 ezlog.log("Engine", "Engine Starting...")
+```
+
+## Write Logs To File
+You can write logs to a file by using either creating a LogFile struct and passing in an 
+os.File or by using `ezlog.open_log_file()`
+```v
+// Open a log file relative to the executable's location
+mut log_file := ezlog.open_log_file("log.txt", "My Log") or { return }
+
+// Do the same functions as with logging to the console, but just append "_file" to the name
+// and pass in a mut reference to os.File
+ezlog.start_section_file("Application", "This is my Heading", mut log_file)
+ezlog.log_file("Application", "Hello, Heading!", mut log_file)
+ezlog.log_file("Application", "Hello, Heading Again!", mut log_file)
+ezlog.end_section_file(mut log_file)
+log_file.close() // Don't forget to close the file after you're done with it!
 ```
--- ezlog.v
+++ ezlog.v
@@ -55,11 +55,19 @@ pub enum ConsoleStyling {
     overlined = 53
 }
 
+@[params]
 pub struct ConsoleTextFormat {
   pub:
      foreground_color    ConsoleForegroundColor = .default
      background_color    ConsoleBackgroundColor
      styling             []ConsoleStyling
+}
+
+pub struct LogFile {
+mut:
+    indentation   int = 0
+pub mut:
+    file          os.File
 }
 
 // print_formatted prints a string of text to the console with a given set of styling conditions.
@@ -81,19 +89,49 @@ pub fn print_formatted(text string, format ConsoleTextFormat) {
     println("${text}\e[0m")
 }
 
+// print_formatted_file prints a string of text to the console with a given set of styling conditions, and
+// writes the text to a given file.
+pub fn print_formatted_file(text string, mut file LogFile, format ConsoleTextFormat) {
+    print_formatted(text, format)
+    file.file.writeln(text) or {  }
+}
+
 // start_section prints a heading in the format "[[caller_name]]: [message]", and
 // indents any following calls to `log()`, `log_message()`, etc. by 1
-pub fn start_section(caller_name string, message string) {
-    log(caller_name, message)
+pub fn start_section(caller_name string, message string, format ConsoleTextFormat) {
+    log(caller_name, message, format)
     unsafe { delta_indentation(1) }
+}
+
+// start_section_file prints a heading in the format "[[caller_name]]: [message]",
+// indents any following calls to `log()`, `log_message()`, etc. by 1 and
+// writes the heading to the given file
+pub fn start_section_file(caller_name string, message string, mut file LogFile, format ConsoleTextFormat) {
+    log_file(caller_name, message, mut file, format)
+    unsafe {
+        delta_indentation(1)
+        file.indentation += 1
+    }
 }
 
 // start_section_heading prints a heading in the format "[message]", and
 // indents any following calls to `log()`, `log_message()`, etc. by 1.
 // This is essentially the same as `start_section`, but without a caller's name needing to be explicitly added
-pub fn start_section_heading(message string) {
-    log_message(message)
+pub fn start_section_heading(message string, format ConsoleTextFormat) {
+    log_message(message, format)
     unsafe { delta_indentation(1) }
+}
+
+// start_section_heading_file prints a heading in the format "[message]",
+// indents any following calls to `log()`, `log_message()`, etc. by 1 and
+// writes the heading to a given file
+// This is essentially the same as `start_section_file`, but without a caller's name needing to be explicitly added
+pub fn start_section_heading_file(message string, mut file LogFile, format ConsoleTextFormat) {
+    log_message_file(message, mut file, format)
+    unsafe {
+        delta_indentation(1)
+        file.indentation += 1
+    }
 }
 
 // end_section ends the messages that should come under whatever heading/subheading name that was made in `start_section` or
@@ -102,72 +140,118 @@ pub fn end_section() {
     unsafe { delta_indentation(-1) }
 }
 
-// log prints a message to the console in the format "[[caller_name]]: [message]" with default console colors,
-// and prints to the file specified in `set_log_file` if it has been set.
-pub fn log(caller_name string, message string) {
-    unsafe { print_indentations(delta_indentation(0)) }
-    print_formatted("[${caller_name}]: ${message}", ConsoleTextFormat{})
-    append_line_to_log_file("[${caller_name}]: ${message}")
+// end_section_file ends the messages that should come under whatever heading/subheading name that was made in `start_section_file` or
+// `start_section_heading_file`, and un-indents any following calls to `log_file()`, `log_message_file()`, 'log()`, etc. by 1.
+pub fn end_section_file(mut file LogFile) {
+    unsafe {
+        delta_indentation(-1)
+        file.indentation -= 1
+    }
 }
 
-// log_message prints a message to the console in the format "[message]" with default console colors,
+// log prints a message to the console in the format "[[caller_name]]: [message]" with specified styling
+pub fn log(caller_name string, message string, format ConsoleTextFormat) {
+    unsafe { print_indentations(delta_indentation(0)) }
+    print_formatted("[${caller_name}]: ${message}", format)
+}
+
+// log_file prints a message to the console in the format "[[caller_name]]: [message]" with specified styling
+// and writes to the file specified.
+pub fn log_file(caller_name string, message string, mut file LogFile, format ConsoleTextFormat) {
+    unsafe {
+        print_indentations(delta_indentation(0))
+        print_indentations_file(file.indentation, mut file)
+    }
+    print_formatted_file("[${caller_name}]: ${message}", mut file, format)
+}
+
+// log_message prints a message to the console in the format "[message]" with specified styling
 // and prints to the file specified in `set_log_file` if it has been set.
 // This is essentially the same as `log`, but without a caller's name needing to be explicitly added
-pub fn log_message(message string) {
+pub fn log_message(message string, format ConsoleTextFormat) {
     unsafe { print_indentations(delta_indentation(0)) }
-    print_formatted(message, ConsoleTextFormat{})
-    append_line_to_log_file(message)
+    print_formatted(message, format)
 }
 
-// log_error prints a message to the console in the format "{ERROR} [[caller_name]]: [message]" in red
+// log_message_file prints a message to the console in the format "[message]" with specified styling
+// and writes to the file specified.
+// This is essentially the same as `log_file`, but without a caller's name needing to be explicitly added
+pub fn log_message_file(message string, mut file LogFile, format ConsoleTextFormat) {
+    unsafe {
+        print_indentations(delta_indentation(0))
+        print_indentations_file(file.indentation, mut file)
+    }
+    print_formatted_file(message, mut file, format)
+}
+
+// log_error prints an error message to the console in the format "{ERROR} [[caller_name]]: [message]" in red
 // and prints to the file specified in `set_log_file` if it has been set.
 // Note that [print_back_trace] *does not* print to the file, just the console.
 pub fn log_error(caller_name string, message string, print_back_trace bool) {
     unsafe { print_indentations(delta_indentation(0)) }
-    print_formatted("{ERROR} [${caller_name}]: ${message}", foreground_color: .red)
-    append_line_to_log_file("{ERROR} [${caller_name}]: ${message}")
+    log_message("{ERROR} [${caller_name}]: ${message}", foreground_color: .red)
 
     if print_back_trace {
         print_backtrace()
     }
 }
 
-// log_error_message prints a message to the console in the format "{ERROR} [message]" in red
+// log_error_file prints an error message to the console in the format "{ERROR} [[caller_name]]: [message]" in red
+// and prints to the file specified in `set_log_file` if it has been set.
+// Note that [print_back_trace] *does not* print to the file, just the console.
+pub fn log_error_file(caller_name string, message string, print_back_trace bool, mut file LogFile) {
+    unsafe {
+        print_indentations(delta_indentation(0))
+        print_indentations_file(file.indentation, mut file)
+    }
+    log_message_file("{ERROR} [${caller_name}]: ${message}", mut file, foreground_color: .red)
+
+    if print_back_trace {
+        print_backtrace()
+    }
+}
+
+// log_error_message prints an error message to the console in the format "{ERROR} [message]" in red
 // and prints to the file specified in `set_log_file` if it has been set.
 // Note that [print_back_trace] *does not* print to the file, just the console.
 // This is essentially the same as `log_error`, but without a caller's name needing to be explicitly added
 pub fn log_error_message(message string, print_back_trace bool) {
     unsafe { print_indentations(delta_indentation(0)) }
-    print_formatted("{ERROR} ${message}", foreground_color: .red)
-    append_line_to_log_file("{ERROR} ${message}")
+    log_message("{ERROR} ${message}", foreground_color: .red)
 
     if print_back_trace {
         print_backtrace()
     }
 }
 
-// log_formatted prints a message to the console in the format "[[caller_name]]: [message]" with custom console formatting
+// log_error_message prints an error message to the console in the format "{ERROR} [message]" in red
 // and prints to the file specified in `set_log_file` if it has been set.
-pub fn log_formatted(caller_name string, message string, format ConsoleTextFormat) {
-    unsafe { print_indentations(delta_indentation(0)) }
-    print_formatted("[${caller_name}]: ${message}", format)
-    append_line_to_log_file("[${caller_name}]: ${message}")
-}
+// Note that [print_back_trace] *does not* print to the file, just the console.
+// This is essentially the same as `log_error`, but without a caller's name needing to be explicitly added
+pub fn log_error_message_file(message string, print_back_trace bool, mut file LogFile) {
+    unsafe {
+        print_indentations(delta_indentation(0))
+        print_indentations_file(file.indentation, mut file)
+    }
+    log_message_file("{ERROR} ${message}", mut file, foreground_color: .red)
 
-// log_message_formatted prints a message to the console in the format "[message]" with custom console formatting
-// and prints to the file specified in `set_log_file` if it has been set.
-// This is essentially the same as `log_formatted`, but without a caller's name needing to be explicitly added
-pub fn log_message_formatted(message string, format ConsoleTextFormat) {
-    unsafe { print_indentations(delta_indentation(0)) }
-    print_formatted(message, format)
-    append_line_to_log_file(message)
+    if print_back_trace {
+        print_backtrace()
+    }
 }
 
 fn print_indentations(count int) {
     for _ in 0..count {
         unsafe {
             print(set_indentation_text_unsafe(""))
-            append_text_to_log_file(set_indentation_text_unsafe(""))
+        }
+    }
+}
+
+fn print_indentations_file(count int, mut file LogFile) {
+    for _ in 0..count {
+        unsafe {
+            file.file.write_string(set_indentation_text_unsafe("")) or {  }
         }
     }
 }
@@ -199,58 +283,19 @@ fn delta_indentation(delta int) int {
     return indentation
 }
 
-// set_log_file sets a file that all calls to `log`, `log_message`, etc. will also write to, so it can be read over later.
-// If this is never set, no log file will be made and all calls will simply output to the console.
-pub fn set_log_file(file_name string, heading string) {
-    unsafe { set_opened_file(file_name, heading) }
+// open_log_file opens a file relative to the executable and writes the heading.
+// This returns an os.File that can then be used to write logs to.
+// *Don't forget to call `close()` on the file!*
+pub fn open_log_file(log_file_name string, heading string) !LogFile {
+    mut log_file := os.open_file(os.dir(os.executable()) + "/${log_file_name}", "w", 0)!
+    log_file.writeln("=== ${heading} ===")!
+    return LogFile {
+        indentation: 0
+        file: log_file
+    }
 }
 
-@[unsafe]
-fn set_opened_file(file_name string, heading string) int {
-    mut static opened_file_name := ""
-    mut static opened_fd := -1
-
-    if (file_name == "" || file_name == opened_file_name) && opened_fd != -1 {
-        return opened_fd
-    } else if (file_name != "" && file_name != opened_file_name) && opened_fd != -1
-    {
-        // If file was previously opened, close it so another one can be opened
-        os.fd_close(opened_fd)
-    }
-
-    if file_name != "CLOSE_FILE" {
-        println("=== Writing log file to: ${os.dir(os.executable())}/${file_name} ===")
-        opened_file := os.open_file("${os.dir(os.executable())}/${file_name}", "w") or { return -1  }
-        opened_fd = opened_file.fd
-        opened_file_name = file_name
-        append_line_to_log_file(heading)
-    } else {
-        return -1
-    }
-
-    return opened_fd
-}
-
-fn append_text_to_log_file(text string) {
-    mut fd := unsafe { set_opened_file("", "") } // If no file opened, simply return
-
-    if fd == -1 {
-        return
-    }
-
-    os.fd_write(fd, text)
-}
-
-fn append_line_to_log_file(text string) {
-    mut fd := unsafe { set_opened_file("", "") } // If no file opened, simply return
-
-    if fd == -1 {
-        return
-    }
-
-    os.fd_write(fd, "${text}\n")
-}
-
-fn cleanup() {
-    unsafe { set_opened_file("CLOSE_FILE", "") } // Closes the file logs are being written to
+// close closes the log file and allows other programs to access it
+pub fn (mut file LogFile) close() {
+    file.file.close()
 }
